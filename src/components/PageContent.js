42ce6b3c9fa2de7965bfdd49467c328824890d91
Pc <kristaptravinsshop@aol.com>
2022-05-22 19:28:58 +0300

    edit_animation

--- src/components/PageContent.js
+++ src/components/PageContent.js
@@ -18,17 +18,17 @@ return (
 >
     <div onMouseEnter={()=>setEntered(true)}
          onClick={()=>setOpen(!isOpen)} 
-         className={`${isOpen?"open_img_1":""}image_box_1 ${isEntered?"active":""}`}>
+         className={`${isOpen?"open_img_1":""} image_box_1 ${isEntered?"active":""}`}>
       <img src={props.image1} alt="" />
     </div>
     <div onMouseEnter={()=>setEntered(true)}
          onClick={()=>setOpen(true)} 
-         className={`image_box_2 ${isEntered?"active1":""}`}>
+         className={`${isOpen?"open_img_2":""} image_box_2 ${isEntered?"active1":""}`}>
       <img src={props.image2} alt="" />
     </div>
     <div onMouseEnter={()=>setEntered(true)}
          onClick={()=>setOpen(true)} 
-         className={`image_box_3${isEntered?" active2":""}`}>
+         className={`${isOpen?"open_img_3":""} image_box_3${isEntered?" active2":""}`}>
       <img src={props.image3} alt="" />
     </div>
     {isOpen?<div className={`${isOpen?"full_view_img1":""} image_box_4 `}><img src={props.image4} alt="" /></div>:<></>}
@@ -40,8 +40,8 @@ return (
       {width>=900?!isOpen?<TitleTextWide title_text={props.title_text} />:<></>:!isOpen?< TitleTextMob title_text={props.title_text} />:<></>}
     </div>
 
-    {isOpen?<div className='info_text'><p>{props.open_text}</p></div>:<></>}
-    {isOpen?<div className='info_text2' ><p>{props.open_text1}</p></div>:<></>}
+    {isOpen?<div className={`${isOpen?"open_info_text":""} info_text`}><p>{props.open_text}</p></div>:<></>}
+    {isOpen?<div className={`${isOpen?"open_info_text2":""} info_text2`} ><p>{props.open_text1}</p></div>:<></>}
 </div>
   )
 }
