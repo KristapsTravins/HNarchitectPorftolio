a5935edf5ace887342e647a03403707483b2f9bc
KristapTravins <krsitaptravins@gmail.com>
2022-05-27 11:56:10 +0300

    clean_s

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
 import {MenuBtn} from './components/MenuBtn';
-import MenuOverlay from './components/MenuOverlay';
 import { useState } from 'react';
-import Home from'./pages/Home';
-
 import './css/animate.css';
 import { Routes ,Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
-  const [MenuState, setMenuState] = useState(false);
-  const [animatedState,setAnimatedState] = useState('closedStance');
-
-
-  const SwitchMenu =()=>{
-    if(!MenuState){
-      setAnimatedState("openMenu");
-      setMenuState(!MenuState);
-    }else{
-      setAnimatedState("closeMenu");
-      setMenuState(!MenuState); 
-    }
-
-  }
-
   return (
     <BrowserRouter>
     <div  className="App">
-    <MenuOverlay closeMenu={SwitchMenu} menuState={animatedState}/>
-    <MenuBtn closeMenu={SwitchMenu} menuState={MenuState} />
-    <div className='content-block'>
     <Routes>
-    <Route path='/' element={<Home/>} />
+    <Route path='/' element={<></>} />
     </Routes>
-    </div>
     </div>
     </BrowserRouter>
   );
