6e690a2d819807366182eda6cbd2f75da41ddc95
Pc <kristaptravinsshop@aol.com>
2022-06-05 11:09:24 +0300

    title description animationV2

--- src/components/Project_title_description/ProjectDescription.js
+++ src/components/Project_title_description/ProjectDescription.js
@@ -3,19 +3,21 @@ import "./css/global.css"
 import { motion } from "framer-motion"
 
  const ProjectDescription =(props)=> {
-let counter =1.5;
+const counters = [0.1,0.4,0.5,0.7,0.9,1]
+let i = 0;
 return (
 <motion.div
           className='huts_title_info'
           >
             {props.titleDescription.map((p)=>{
+            
               return(
                <div className='list_item'>
                   <div className='left'>[ </div>
                     <motion.div className='data'
                     initial={{width:"0px"}}
                     animate={{width:"auto"}}
-                    transition={{duration:1.2,delay:counter++}}
+                    transition={{duration:0.7,delay:counters[i++]}}
                     >{p}</motion.div>
                  
                   <div className='right'>]</div>
