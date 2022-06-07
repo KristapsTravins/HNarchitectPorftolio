7a9a410be01430404224066847fb6bf45c885de3
KristapTravins <krsitaptravins@gmail.com>
2022-06-07 15:35:49 +0300

    hover animation

--- src/components/Project_hut/ProjectHut.js
+++ src/components/Project_hut/ProjectHut.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { motion } from "framer-motion"
+import React, { useState } from 'react'
+import { motion, transform } from "framer-motion"
 import useWindowDimensions from '../../assets/hooks/useWindowDimensions'
 import {AImg1,AImg2,AImg3,AImg4,AImg5} from "../../assets/images/ImageComponents"
 import ProjectTitle from '../Project_title/ProjectTitle'
 import ProjectDescription from '../Project_title_description/ProjectDescription'
-
+import animData from "./animationData/animationData.json"
 
 import "./css/mobile.css";
 import "./css/tab.css";
@@ -12,30 +12,39 @@ import "./css/desktop.css";
 
 
 const ProjectHut =(props)=> {
-  console.log(props.sectionOpen)
   const { width } = useWindowDimensions();
-  console.log(width)
+  const [hover,setHover] = useState(false);
+  console.log(animData);
+  
   return (
     <motion.div className='project_huts_main'>
       <div className='project_huts_inner'>
         <div className='project_huts_trigger_box'
-        
+        onMouseEnter={()=>setHover(true)}
+        onMouseLeave={()=>setHover(false)}
         ></div>
           <ProjectTitle />
           <motion.div
             className='huts_img1'
+            animate={hover?animData.HoverAnimations.img1.move:{}}
+            transition={animData.HoverAnimations.img1.transitions}
             >
             <AImg2/>
           </motion.div>
 
           <motion.div
             className='huts_img2'
+            animate={hover?animData.HoverAnimations.img2.move:{}}
+            transition={animData.HoverAnimations.img2.transitions}
             >
             <AImg1/>
           </motion.div>
 
           <motion.div
             className='huts_img3'
+            animate={hover?animData.HoverAnimations.img3.move:{}}
+            transition={animData.HoverAnimations.img3.transitions}
+            
             >
             <AImg3/>
           </motion.div>
---
+++ src/components/Project_hut/hooks/hoverStateHook.js
@@ -0,0 +1,6 @@
+import { useState, useEffect } from 'react';
+import animData from "../animationData/animationData.json"
+
+export default HoverAnimation =(screenWidth)=>{
+
+}
