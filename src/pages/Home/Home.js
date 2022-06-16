8102da9d9890773cec9a2c97d48c83fc83621ee1
Pc <kristaptravinsshop@aol.com>
2022-06-16 20:00:50 +0300

    newLayout Clean

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import Project from "../../components/Project_hut/ProjectHut";
+
 import { useState } from "react";
 import "./css/global.css"
 
@@ -7,7 +7,6 @@ const Home = () => {
   const [sectionState,setSectionState] = useState(false);
     return (
      <div className="home_center">
-       <Project sectionOpen={{sectionState,setSectionState}} titleDescriptionData={['Modular prefab home','Concept','2022','Latvia','40 sq.m.']} />
 
      </div>
     );
