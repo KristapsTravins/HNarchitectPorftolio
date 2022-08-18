db6ad7564cd633c0fe8d40a00aabbd4a8ccff989
Pc <kristaptravinsshop@aol.com>
2022-08-18 22:32:19 +0300

    ready edit openClose

--- src/components/Project_components/Project_hut/ProjectHut.js
+++ src/components/Project_components/Project_hut/ProjectHut.js
@@ -19,37 +19,40 @@ const ProjectHut = (props) => {
    
     <motion.div 
          className='img_1'
+         style={{"border":"1px solid black"}}
          animate={{
             y:45,
             opacity:1
           }}
           transition={{ ease: "easeIn", duration: 1.4 }}
          >
-            <AImg1 />   
+          {/*   <AImg1 />    */}
     </motion.div>
     <TriggerBox  />
     <motion.div 
          className='img_2'
+         style={{"border":"1px solid black"}}
          animate={{
            x:35,
            opacity:1
           }}
           transition={{ ease: "easeIn", duration: 1.4, delay:1 }}
          >   
-            <AImg2 />
+         {/*    <AImg2 /> */}
     </motion.div>
 
    
 
     <motion.div 
          className='img_3'
+         style={{"border":"1px solid black"}}
          animate={{
            x:55,
            opacity:1
           }}
           transition={{ ease: "easeIn", duration: 1.4, delay:2 }}
          >   
-            <AImg3 />
+           {/*  <AImg3 /> */}
     </motion.div>
 
     </div>
