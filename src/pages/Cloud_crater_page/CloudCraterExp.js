1fb78c2bca743d70a887227245da5a0d19d5dbcf
Pc <kristaptravinsshop@aol.com>
2022-09-08 14:14:40 +0300

    sec section open  work2

--- src/pages/Cloud_crater_page/CloudCraterExp.js
+++ src/pages/Cloud_crater_page/CloudCraterExp.js
@@ -6,9 +6,14 @@ import { motion } from "framer-motion"
 import { BImg1,BImg2,BImg3,BImg4,BImg5,BImg6,BImg7  } from '../../assets/images/ImageComponents'
 import ProjectTitle from '../../components/ProjectTitle/ProjectTitle'
 import ProjectDescription from '../../components/Project_title_description/ProjectDescription'
+import ProjectTextDescription from '../../components/Project_description/ProjectTextDescription'
 
 import './css/main.css'
 import './css/view.css'
+
+const text1 = "<p>Cloud Catcher aims to address the ever rising <span>global water crisis.</span> It is estimated that <span>2.7 billion people worldwide suffer due to water scarcity.</span> Some of the affected regions don't get any rainfall for months or even years, most of the precipitation simply stays in the air, but <span>there also lies an opportunity to collect drinking water out of thin air</span> using biomimicry.</p>"
+
+const text2 = "The whole facade is covered in <span>recycled polyethylene mesh</span> which <span>catches</span> the air embodied <span>water</span> as <span>condensation</span>. Further it is stored in transparent <span>water-wall</span> tanks. This solution saves space, creates a lot of backup water storage and functions as a <span>thermal regulator</span>, thanks to water's large thermal storage capacity."
 
 
 const CloudCraterExp = () => {
@@ -36,9 +41,15 @@ const CloudCraterExp = () => {
             
 
             <motion.div className='img_1'>
-            <BImg2 />   
-            </motion.div>  
+                <BImg2 />   
+            </motion.div>
 
+            <motion.div className='img_2'>
+                <BImg1 />   
+            </motion.div>
+
+            <ProjectTextDescription text={text2} textClass={"text_description_2"} />
+            <ProjectTextDescription text={text1} textClass={"text_description_1"} />
 
 
 
