ac1a39509468bc7b050f6dbb14db2548340defcd
same guy <kristaps@k.com>
2022-11-25 18:36:38 +0200

    start

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./styleVars/styleShorcuts.scss"
+import "./main_style.scss"
+
+const App =()=>{
+
+    return(
+        <div className="app">
+            <div className="base_center">
+
+            </div>
+        </div>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Hello from "./Hello";
-import Test from "./components/testComp/Testthis";
+import App from "./App";
+
+
+
+
+
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<Test />);
+root.render(<App/>);
